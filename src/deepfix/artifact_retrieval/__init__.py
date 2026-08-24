391cbdc14c8688477512d7bda023568cc7729d6a
TCM_construction <1410537399@qq.com>
2026-08-24 16:46:22 +0800

    feat: expose diagnostic artifact tools

--- src/deepfix/artifact_retrieval/__init__.py
+++ src/deepfix/artifact_retrieval/__init__.py
@@ -13,6 +13,10 @@ from deepfix.artifact_retrieval.models import (
     stable_diagnostic_artifact_id,
 )
 from deepfix.artifact_retrieval.service import DiagnosticArtifactService
+from deepfix.artifact_retrieval.tools import (
+    build_read_diagnostic_artifact_tool,
+    build_search_diagnostic_artifacts_tool,
+)
 
 __all__ = [
     "ArtifactReferenceCollector",
@@ -25,5 +29,7 @@ __all__ = [
     "DiagnosticMatch",
     "DiagnosticReadResult",
     "DiagnosticSearchResult",
+    "build_read_diagnostic_artifact_tool",
+    "build_search_diagnostic_artifacts_tool",
     "stable_diagnostic_artifact_id",
 ]
